334c325b54d31025b53993f9b97fd5b539bffca7
Mathias Lorenzen <mlo@dandomain.dk>
2017-10-03 11:27:00 +0200

    added test for mail sender.

---
+++ src/TestabilityKata.Tests/MailSenderTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestabilityKata.Tests
+{
+    [TestClass]
+    public class MailSenderTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MailSenderThrowsExceptionIfEmailIsInvalid()
+        {
+            var mailSender = new MailSender();
+            mailSender.SendMail("my-invalid-email", "some content");
+        }
+    }
+}
